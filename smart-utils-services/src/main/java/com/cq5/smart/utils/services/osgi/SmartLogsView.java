4d6e5b99cbe784b30a913ce4441060a2a93167f6
Andrew Manuev <amanuev@gmail.com>
2013-05-15 13:32:10 +0300

    Add todo list. Add basic template. Create a utils for resource retrieving. Add i18n properties.

--- smart-utils-services/src/main/java/com/cq5/smart/utils/services/osgi/SmartLogsView.java
+++ smart-utils-services/src/main/java/com/cq5/smart/utils/services/osgi/SmartLogsView.java
@@ -16,24 +16,28 @@ import javax.servlet.http.HttpServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-//TODO: smart logger should ask a service fol log file by name and output it to browser
-//TODO: smart logger should have ability to archive a selected log file and send it to user
-//TODO: smart logger should have ability a output only line which contain a part of string
-//TODO: smart logger should have ability to select string with part of string and archive it and give it to user
 @Component
 @Service(Servlet.class)
 @Properties({
         @Property(name = WebConsoleConstants.PLUGIN_LABEL, value = "smartlogger"),
         @Property(name = WebConsoleConstants.PLUGIN_TITLE, value = "Smart Logger"),
-        @Property(name = WebConsoleConstants.PLUGIN_CSS_REFERENCES, value = "/res/ui/smart_logger.css")
+        @Property(name = WebConsoleConstants.PLUGIN_CSS_REFERENCES, value = "/resources/templates/smart-utils/css/smart-utils.css")
 })
 public class SmartLogsView extends HttpServlet {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SmartLogsView.class);
 
+    private static final String LOG_PARAM = "log";
+
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-        PrintWriter writer = res.getWriter();
-        writer.print("This is a test plugin for Felix web console");
+        renderContent(res);
     }
+
+    private void renderContent(ServletResponse res) throws IOException {
+        PrintWriter writer = res.getWriter();
+        String template = ResourceUtils.readTemplateFile(getClass(), "/templates/smart-utils/smart-utils.html");
+        writer.println(template);
+    }
+
 }
